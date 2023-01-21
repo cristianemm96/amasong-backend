a42c33795f812abae7ec3b70e3fe06e72ca36c85
Cristian Mariño <cristian.emanuel.m96@gmail.com>
2023-01-21 19:33:23 -0300

    fix mailer

--- config/mailer.js
+++ config/mailer.js
@@ -1,18 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer  from "nodemailer";
+import nodemailer from "nodemailer";
 // create reusable transporter object using the default SMTP transport
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,            
-  secure: false,  // true for 465, false for other ports
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: "cristian.emanuel.96@gmail.com", // generated ethereal user
     pass: process.env.MAILERPASSWORD, // generated ethereal password
-  },tls: {
-    secureProtocol: "TLSv1_method"
-}
+  },
+  tls: {
+    secure: false,
+    ignoreTLS: true,
+    rejectUnauthorized: false,
+  },
 });
 
-transporter.verify().then(() => console.log("Mailer OK")).catch(console.log("Error al conectar"));
+transporter
+  .verify()
+  .then(() => console.log("Mailer OK"))
+  .catch(console.log("Error al conectar"));
