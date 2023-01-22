ed7e18269cfb06606d6764344c2a077e4d507af4
Cristian Mariño <cristian.emanuel.m96@gmail.com>
2023-01-21 22:06:45 -0300

    fix mailer

--- config/mailer.js
+++ config/mailer.js
@@ -3,10 +3,7 @@ import nodemailer from "nodemailer";
 dotenv.config();
 // create reusable transporter object using the default SMTP transport
 export const transporter = nodemailer.createTransport({
-  //host: "smtp.gmail.com",
   service: 'gmail',
-  //port: 587,
-  //secure: false, // true for 465, false for other ports
   auth: {
     user: "cristian.emanuel.96@gmail.com", // generated ethereal user
     pass: `${process.env.MAILERPASSWORD}`, // generated ethereal password
